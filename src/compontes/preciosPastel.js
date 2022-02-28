25d3978259b914d310ae8ce58e3db12331c6448f
hrrRafael2 <rafaelhruiz@gmail.com>
2022-02-27 22:32:58 -0600

    add: js

---
+++ src/compontes/preciosPastel.js
@@ -0,0 +1,12 @@
+/**  */
+const pastelChocolate = Vue.createApp({
+    data () {
+        return {
+            name: 'Pastel de chocolate',
+            precio: 120
+        }
+        
+    }
+
+})
+pastelChocolate.mount('#pastelesPrecios')
